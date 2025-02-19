675c9caa8ff2c80c925db539a50edd7ef8c05764
Aaron Reisman <aaronreisman@sort.xyz>
2025-02-19 13:25:44 -0800

    chore: add faulty route

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,8 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("about", "routes/about.tsx"),
+  layout("layouts/example.tsx", [
+    route("about", "routes/about+test.tsx")
+  ]),
 ] satisfies RouteConfig;
